374afab5bf9e63499ecb61838178c2ced0048ad6
jay <jaylong255@gmail.com>
2021-03-09 11:28:46 -0600

    adding user route

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,10 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Lcobucci\JWT\Exception;
 
 class UserController extends Controller
 {
     public function index(){
         return "word1234";
+    }
+    public function user(Request $request){
+//        return $request->user();
+        try {
+//            return $request->user();
+            return response()->json(['message' => ]);
+        } catch (Exception $e){
+            return response()->json(['error'=>$e->getMessage()]);
+        }
+
     }
 }
